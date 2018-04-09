de5d7a7c8220c91f3e3ddd3e4fb946a0bc4d7418
Vijay <vijayvijay992@gmail.com>
2018-04-09 21:20:38 +0530

    some code modified

---
+++ app/src/main/java/com/example/vijay/myapplication/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.vijay.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView t =(TextView)findViewById(R.id.id_tv);
+        t.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "firstddd application d", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
